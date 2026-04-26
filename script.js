9b1142d43293bd401f3983ab97310f3ba4a2aa6c
youssef18 <youssefodada11@gmail.com>
2026-04-26 02:46:02 +0100

    creating the logic of the sign in form still working on log in valditation

--- script.js
+++ script.js
@@ -2,35 +2,83 @@ const signInForm = document.getElementById("sign-In-form");
 const nameInput = document.getElementById("input-name");
 const emailInput = document.getElementById("email-input");
 const passwordInput = document.getElementById("password-input");
-const rege = document.querySelector(".rege");
-const userData = [];
+const card = document.getElementById("card");
+const logInForm = document.querySelector(".regestirForm");
+const logInEmail = document.getElementById("login-email-Input");
+const logInPassword = document.getElementById("login-password-Input");
+
+const userData = JSON.parse(sessionStorage.getItem("userData")) || [];
+
 function users(name, email, password) {
   this.name = name;
   this.email = email;
   this.password = password;
 }
 
-function creatuser(name, email, password) {
+function createUser(name, email, password) {
   const user = new users(name, email, password);
+  if (userData.some((user) => user.email === emailInput.value))
+    return alert("this email alerady taken");
   userData.push(user);
+  sessionStorage.setItem("userData", JSON.stringify(userData));
+  signInForm.reset();
+  window.location.href = "dashboard.html";
 }
-function displaycard(userData) {
-  userData.forEach((user) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<span class="card-image">HR</span>
-    <h3 class="user-name">${user.name}</h3>
-    <p class="user-email">${user.email}</p>
-    <p>عرض مبسّط للمستخدم — الاسم والبريد الإلكتروني فقط</p>`;
-    rege.appendChild(card);
-    return card;
+
+// Sign-in page
+if (signInForm) {
+  signInForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createUser(
+      nameInput.value,
+      emailInput.value,
+      passwordInput.value,
+      signInForm,
+    );
   });
 }
-signInForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  creatuser(nameInput.value, emailInput.value, passwordInput.value);
-  displaycard(userData);
-  signInForm.reset();
-});
-creatuser("youssef", "youssefodada11@gmail.com", "youssefodadad123");
+
+// Dashboard page
+if (card) {
+  userData.forEach((user) => {
+    card.innerHTML = `
+        <span class="card-image">HR</span>
+        <h3 class="user-name">${user.name}</h3>
+        <p class="user-email">${user.email}</p>
+        <p>عرض مبسّط للمستخدم — الاسم والبريد الإلكتروني فقط</p>`;
+  });
+}
+
+//Log in page
+function enterUser(email, password) {
+  const savedUsers = JSON.parse(sessionStorage.getItem("userData")) || [];
+  const loginuser = savedUsers.find(
+    (user) => user.email === email && user.password === password,
+  );
+  if (loginuser) {
+    sessionStorage.setItem("currentUser", JSON.stringify(loginuser));
+    window.location.href = "dashboard.html";
+  } else {
+    alert("this user not found you need to creat new accounr");
+  }
+
+  logInForm.reset();
+
+  if (card) {
+    if (savedUsers) {
+      card.innerHTML = `
+      <span class="card-image">HR</span>
+      <h3 class="user-name">${loginuser.name}</h3>
+      <p class="user-email">${loginuser.email}</p>
+      <p>عرض مبسّط للمستخدم — الاسم والبريد الإلكتروني فقط</p>`;
+    }
+  }
+}
+
+if (logInForm) {
+  logInForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    enterUser(logInEmail.value, logInPassword.value);
+  });
+}
 console.log(userData);
